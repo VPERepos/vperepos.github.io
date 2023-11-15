48af1085ed4af18405ef7e11a90ae236c6b583c6
Vladimir Petukhov <v.petukhov@mail.de>
2023-11-15 17:58:12 +0600

    deploy

---
+++ docs/deploy.ps1
@@ -0,0 +1,11 @@
+# build
+npm run src:build
+
+cd src/.vuepress/dist
+
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:vperepos/vperepos.github.io.git main
+
+cd -
--- docs/src/Curriculum/README.md
+++ docs/src/Curriculum/README.md
@@ -3,7 +3,7 @@ sidebar: auto
 ---
 # Resume
 ## English version
-
+Here you can read the english version of my resume.
 ## German version
 
 ## Russian version
