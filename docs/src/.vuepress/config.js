b0c8800fb183798943321a306bd800842f943e5f
Vladimir Petukhov <v.petukhov@mail.de>
2024-04-16 12:52:48 +0500

    Added skeleton for knowledge base

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -1,74 +1,78 @@
 const { description } = require('../../package')
 
 module.exports = {
-  title: 'Dr. Vladimir Petukhov',
-  description: 'A portfolio site as a general C++/Python programmer, general algorithms developer, industrial computer vision engineer, machine learning engineer and web apps developer.',
-  head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    [
-      'script',
-      {
-          async: true,
-          src: 'https://www.googletagmanager.com/gtag/js?id=G-6HX7S00F14',
-      },
-    ],
-    [
-        'script',
-        {},
+    title: 'Dr. Vladimir Petukhov',
+    description: 'A portfolio site as a general C++/Python programmer, general algorithms developer, industrial computer vision engineer, machine learning engineer and web apps developer.',
+    head: [
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
         [
-            "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-6HX7S00F14');",
+            'script',
+            {
+                async: true,
+                src: 'https://www.googletagmanager.com/gtag/js?id=G-6HX7S00F14',
+            },
+        ],
+        [
+            'script',
+            {},
+            [
+                "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-6HX7S00F14');",
+            ],
         ],
     ],
-  ],
 
-  themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
-    nav: [
-      {
-        text: 'Projects',
-        link: '/Projects/',
-      },
-      {
-        text: 'Curriculum Vitae',
-        link: '/Curriculum/',
-      },
-      {
-        text: 'Computer Science Skills',
-        link: '/ComputerScienceSkills/',
-      },
-      {
-        text: 'Publications',
-        link: '/Publications/',
-      },
-      {
-        text: 'Certifications',
-        link: '/Certifications/',
-      },
-      {
-        text: 'Readings',
-        link: '/Readings/',
-      },
-      {
-        text: 'Contact and Availability',
-        link: '/Contact/'
-      },
-      {
-        text: 'Github',
-        link: 'https://github.com/vperepos'
-      }
-    ],
-    
-  },
+    themeConfig: {
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: false,
+        nav: [
+            {
+                text: 'Projects',
+                link: '/Projects/',
+            },
+            {
+                text: 'Knowledge Base',
+                link: '/KnowledgeBase/',
+            },
+            {
+                text: 'Curriculum Vitae',
+                link: '/Curriculum/',
+            },
+            {
+                text: 'Computer Science Skills',
+                link: '/ComputerScienceSkills/',
+            },
+            {
+                text: 'Publications',
+                link: '/Publications/',
+            },
+            {
+                text: 'Certifications',
+                link: '/Certifications/',
+            },
+            {
+                text: 'Readings',
+                link: '/Readings/',
+            },
+            {
+                text: 'Contact and Availability',
+                link: '/Contact/'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/vperepos'
+            }
+        ],
 
-  plugins: [
-    '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-    '@vuepress/plugin-google-analytics@next'
-  ]
+    },
+
+    plugins: [
+        '@vuepress/plugin-back-to-top',
+        '@vuepress/plugin-medium-zoom',
+        '@vuepress/plugin-google-analytics@next'
+    ]
 }
---
+++ docs/src/KnowledgeBase/README.md
@@ -0,0 +1,12 @@
+---
+sidebar: auto
+---
+# Knowledge Base
+
+## Possible interview questions.
+### Interview questions for algorithms and data structures.
+### Interview questions for general C++.
+### Interview questions for Python.
+### Interview questions for computer vision.
+### Interview questions for machine learning.
+### Interview questions for software engineering, architecture and design.
