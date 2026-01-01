1cac742ba3727d7fc2a80149afbf833451bb1123
Vladimir Petukhov <v.petukhov@mail.de>
2026-01-01 21:22:40 +0100

    added vuepress installation to the project

--- docs/src/.vuepress/.temp/internal/routes.js
+++ docs/src/.vuepress/.temp/internal/routes.js
@@ -10,7 +10,24 @@ export const routes = Object.fromEntries([
   ["/Blog/state-machine-design-pattern.html", { loader: () => import(/* webpackChunkName: "Blog_state-machine-design-pattern.html" */"C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/Blog/state-machine-design-pattern.html.js"), meta: {"title":"State Machine Design Pattern in Modern C++"} }],
   ["/Blog/task-queue-design-pattern.html", { loader: () => import(/* webpackChunkName: "Blog_task-queue-design-pattern.html" */"C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/Blog/task-queue-design-pattern.html.js"), meta: {"title":"Task Queue Design Pattern in Modern C++"} }],
   ["/Contact/", { loader: () => import(/* webpackChunkName: "Contact_index.html" */"C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/Contact/index.html.js"), meta: {"title":"Contact"} }],
-  ["/Resume/", { loader: () => import(/* webpackChunkName: "Resume_index.html" */"C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/Resume/index.html.js"), meta: {"title":"Curriculum Vitae"} }],
   ["/Services/", { loader: () => import(/* webpackChunkName: "Services_index.html" */"C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/Services/index.html.js"), meta: {"title":"Services"} }],
+  ["/Resume/", { loader: () => import(/* webpackChunkName: "Resume_index.html" */"C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/Resume/index.html.js"), meta: {"title":"Curriculum Vitae"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateRoutes) {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+  }
+  if (__VUE_HMR_RUNTIME__.updateRedirects) {
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ routes, redirects }) => {
+    __VUE_HMR_RUNTIME__.updateRoutes(routes)
+    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
+  })
+}
--- docs/src/.vuepress/.temp/pages/Blog/index.html.js
+++ docs/src/.vuepress/.temp/pages/Blog/index.html.js
@@ -1,3 +1,16 @@
 import comp from "C:/WORKSPACE/Personal/Softwareentwicklung/VpeFreelancerPage/vperepos.github.io/docs/src/.vuepress/.temp/pages/Blog/index.html.vue"
 const data = JSON.parse("{\"path\":\"/Blog/\",\"title\":\"Blog\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Blog\",\"sidebarDepth\":2,\"description\":\"Articles on data science and software engineering.\",\"head\":[[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"Blog\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":\\\"2025-12-31T17:27:39.000Z\\\",\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"Dr. Vladimir Petukhov\\\"}]}\"],[\"meta\",{\"property\":\"og:url\",\"content\":\"https://vperepos.github.io/Blog/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"Dr. Vladimir Petukhov\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"Blog\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"Articles on data science and software engineering.\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"en-US\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2025-12-31T17:27:39.000Z\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2025-12-31T17:27:39.000Z\"}]]},\"git\":{\"updatedTime\":1767202059000,\"contributors\":[{\"name\":\"Vladimir Petukhov\",\"username\":\"\",\"email\":\"v.petukhov@mail.de\",\"commits\":2}],\"changelog\":[{\"hash\":\"00e7e5c1dca2dbd1a7ca8381f9ef8ed2e320199a\",\"time\":1767202059000,\"email\":\"v.petukhov@mail.de\",\"author\":\"Vladimir Petukhov\",\"message\":\"migrated to vuepress 2\"},{\"hash\":\"0b1b33559df802af3d993e140b0127033fe2f2e5\",\"time\":1766679732000,\"email\":\"v.petukhov@mail.de\",\"author\":\"Vladimir Petukhov\",\"message\":\"Initial commit\"}]},\"filePathRelative\":\"Blog/README.md\"}")
 export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
